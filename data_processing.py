90ce73f97cee919be8eb7b9f3a49165d08206ffc
Chaiyapat KUMTHO <scienceteam48036@gmail.com>
2024-11-06 00:31:25 +0700

    update some data_processing code

--- data_processing.py
+++ data_processing.py
@@ -15,83 +15,37 @@ with open(os.path.join(__location__, 'Countries.csv')) as f:
     for r in rows:
         countries.append(dict(r))
 
-# Print the average temperature of all the cities
-print("The average temperature of all the cities:")
-temps = []
-for city in cities:
-    temps.append(float(city['temperature']))
-print(sum(temps)/len(temps))
-print()
-
-# Print all cities in Italy
-cities_temp = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        cities_temp.append(city['city'])
-print("All the cities in", my_country, ":")
-print(cities_temp)
-print()
-
-# Print the average temperature for all the cities in Italy
-# Write code for me
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The average temperature of all the cities in", my_country, ":")
-print(sum(temps)/len(temps))
-print()
-
-# Print the max temperature for all the cities in Italy
-# Write code for me
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The max temperature of all the cities in", my_country, ":")
-print(max(temps))
-print()
-
-# Print the min temperature for all the cities in Italy
-# Write code for me
-temps = []
-my_country = 'Italy'
-for city in cities:
-    if city['country'] == my_country:
-        temps.append(float(city['temperature']))
-print("The min temperature of all the cities in", my_country, ":")
-print(min(temps))
-print()
-
-# Let's write a function to filter out only items that meet the condition
-# Hint: condition will be associated with an anonymous function, e.x., lamdbda x: max(x)
-def filter(condition, dict_list):
-    filtered_list = []
-    for item in dict_list:
-        if condition(item):
-            filtered_list.append(item)
-    return filtered_list
-
-x = filter(lambda x: float(x['latitude']) >= 60.0, cities)
-for item in x:
-    print(item)
-
-# Let's write a function to do aggregation given an aggregation function and an aggregation key
-def aggregate(aggregation_key, aggregation_function, dict_list):
-    temp = []
-    for item in dict_list:
-        temp.append(float(item[aggregation_key]))
-    return aggregation_function(temp)
-
-# Let's write code to
-# - print the average temperature for all the cities in Italy
-print(aggregate('temperature', lambda x: sum(x) / len(x), filter(lambda x: x['country'] == 'Italy', cities)))
-# - print the average temperature for all the cities in Sweden
-print(aggregate('temperature', lambda x: sum(x) / len(x), filter(lambda x: x['country'] == 'Sweden', cities)))
-# - print the min temperature for all the cities in Italy
-print(aggregate('temperature', lambda x: min(x), filter(lambda x: x['country'] == 'Italy', cities)))
-# - print the max temperature for all the cities in Sweden
-print(aggregate('temperature', lambda x: max(x), filter(lambda x: x['country'] == 'Sweden', cities)))
+###############################################################################
+class TableDB:
+    def __init__(self):
+        self.table_database = []
+        
+    def insert(self, table):
+        self.table_database.append(table)
+        
+    def search(self, table_name):
+        for item in self.table_database:
+            if item == table_name:
+                return item
+            return None
+    
+class Table:
+    def __init__(self, table_name, table):
+        self.table_name = table_name
+        self.table = table
+        
+    def filter(self, condition):
+        filter_list = []
+        for item in self.table_name:
+            if condition(item):
+                filter_list.append(item)
+        return filter_list
+    
+    def aggregate(self, aggregation_function, aggregation_key):
+        value = []
+        for item in self.table_name:
+            value.append(float(item[aggregation_key]))
+        return aggregation_function(value)
+    
+    def __str__(self):
+        pass
